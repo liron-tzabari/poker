226039953f2cb6d715c5d01737dc01d0731c4af5
Liron Tzabari <liron.tzabari@hp.com>
2013-12-30 16:51:40 +0200

    client files

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var express = require('express')
   , user = require('./routes/user')
   , http = require('http')
   , path = require('path');
-
+var url = require('url');
 var app = express();
 
 // all environments
@@ -20,7 +20,7 @@ app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'views')));
 
 // development only
 if ('development' == app.get('env')) {
